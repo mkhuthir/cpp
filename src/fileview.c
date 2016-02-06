86eeb3c121c658e541f3e9e4e138305240ab3145
Muthanna A. Attyah <mkhuthir@gmail.com>
2016-02-06 08:09:53 +0400

    Update fileview.c

--- src/fileview.c
+++ src/fileview.c
@@ -6,7 +6,7 @@ int main ( int argc, char *argv[] )
     {
         /* We print argv[0] assuming it is the program name */
 
-        printf( "usage: %s filename", argv[0] );
+        printf( "usage: %s filename\n", argv[0]);
     }
     else 
     {
