e3ce6e4aa9f0a05960417c4b94728e26622599f8
Muthanna A. Attyah <mkhuthir@gmail.com>
2017-01-21 08:52:39 +0400

    test

--- SerialPort/src/main.c
+++ SerialPort/src/main.c
@@ -1,13 +1,18 @@
+// (c) Muthanna A. Attyah 2017
+// Writing/Reading a serial port
+
 #include <stdio.h>
 #include "serialport.h"
 
 int main(void)
 {
+	puts("Connecting to serial port");
 	HANDLE h = openSerialPort("COM1",B9600,one,off);
 
 	char sendbuffer[] = "test";
 	char readbuffer[100];
 
+	puts("Writing to serial port");
 	//write test
 	int bytesWritten = writeToSerialPort(h,sendbuffer,strlen(sendbuffer));
 
