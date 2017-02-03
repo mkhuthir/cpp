404bcea36db6050216e799cd7287e8b77198207a
mkhuthir <mkhuthir@MuthDT2>
2017-02-03 20:57:02 +0400

    Hello project from Ubuntu 16.04 and Neon 2

---
+++ HelloUbuntuNeon/src/HelloUbuntuNeon.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloUbuntuNeon.cpp
+// Author      : Muthanna A. Attyah
+// Version     :
+// Copyright   : (c) Muthanna 2017
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello Muthanna A. Attyah" << endl; // prints Hello Muthanna A. Attyah
+	return 0;
+}
