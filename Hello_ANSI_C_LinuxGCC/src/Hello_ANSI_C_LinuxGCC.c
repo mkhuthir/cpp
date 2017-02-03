c40ec302feb09b5b9e62953f09014deaa6e7c9f5
mkhuthir <mkhuthir@MuthDT2>
2017-02-03 22:22:45 +0400

    ANSI C Hello

---
+++ Hello_ANSI_C_LinuxGCC/src/Hello_ANSI_C_LinuxGCC.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Hello_ANSI_C_LinuxGCC.c
+ Author      : Muthanna A. Attyah
+ Version     :
+ Copyright   : (c) Muthanna 2017
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello Muthanna A. Attyah"); /* prints Hello Muthanna A. Attyah */
+	return EXIT_SUCCESS;
+}
