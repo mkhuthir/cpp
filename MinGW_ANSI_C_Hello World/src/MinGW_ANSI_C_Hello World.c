085bd7ba7271284e23ce42e1d398ec4132515c27
mkhuthir <mkhuthir@MuthPC1>
2017-02-04 18:39:14 +0400

    Added MinGW ANSI C Hello

---
+++ MinGW_ANSI_C_Hello World/src/MinGW_ANSI_C_Hello World.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : MinGW_ANSI_C_Hello.c
+ Author      : Muthanna A. Attyah
+ Version     :
+ Copyright   : (c) Muthanna 2017
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello Muthanna using MinGW Windows ANSI C"); /* prints Hello Muthanna using MinGW Windows ANSI C */
+	return EXIT_SUCCESS;
+}
