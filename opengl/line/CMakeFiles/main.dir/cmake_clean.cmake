c1d9e6e84bb1d379239195802bb8a74cc68f2edd
Muthanna A. Attyah <mkhuthir@gmail.com>
2017-03-06 21:56:39 +0400

    line

--- opengl/line/CMakeFiles/main.dir/cmake_clean.cmake
+++ opengl/line/CMakeFiles/main.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/main.dir/main.c.o"
+  "CMakeFiles/main.dir/main.cpp.o"
   "main.pdb"
   "main"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
+foreach(lang CXX)
   include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
--- opengl/line/main.cpp
+++ opengl/line/main.cpp
@@ -14,7 +14,7 @@ void init(void)           // initialize paramaters
   width  = 800.0;         // GL window width
   height = 800.0;         // GL window height
 
-  ends[0][0] = (int)(0.25*width);  /* (0,0) is the lower left corner */
+  ends[0][0] = (int)(0.25*width);  //  (0,0) is the lower left corner  
   ends[0][1] = (int)(0.75*height);
   ends[1][0] = (int)(0.75*width);
   ends[1][1] = (int)(0.25*height);
@@ -25,7 +25,7 @@ void init(void)           // initialize paramaters
 // Callback functions for GLUT
 
 // Draw the window - this is where all the GL actions are
-void display(void)
+void displayCB(void)
 {
   int i;
 
@@ -43,33 +43,32 @@ void display(void)
 }
 
 // Called when window is resized,also when window is first created, before the first call to display().
-void reshape(int w, int h)
+void reshapeCB(int w, int h)
 {
-  /* save new screen dimensions */
+  // save new screen dimensions
   width = (GLdouble) w;
   height = (GLdouble) h;
 
-  /* tell OpenGL to use the whole window for drawing */
+  // tell OpenGL to use the whole window for drawing
   glViewport(0, 0, (GLsizei) width, (GLsizei) height);
 
-  /* do an orthographic parallel projection with the coordinate
-     system set to first quadrant, limited by screen/window size */
+  // do an orthographic parallel projection with the coordinate system set to first quadrant, limited by screen/window size
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   glOrtho(0.0, width, 0.0, height, -1.f, 1.f);
-
+  
   return;
 }
 
-void kbd(unsigned char key, int x, int y)
+void kbdCB(unsigned char key, int x, int y)
 {
   switch((char)key) {
-  case 'q':
-  case 27:    /* ESC */
-    glutDestroyWindow(wd);
-    exit(0);
-  default:
-    break;
+    case 'q':
+    case 27:    //  ESC  
+      glutDestroyWindow(wd);
+      exit(0);
+    default:
+      break;
   }
 
   return;
@@ -79,35 +78,35 @@ int main(int argc, char *argv[])
 {
   init();
 
-  /* initialize GLUT, let it extract command-line GLUT options that you may provide */
+  //  initialize GLUT, let it extract command-line GLUT options that you may provide  
   glutInit(&argc, argv);
 
-  /* specify the display to be single buffered and color as RGBA values */
+  //  specify the display to be single buffered and color as RGBA values  
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
 
-  /* set the initial window size */
+  //  set the initial window size  
   glutInitWindowSize((int) width, (int) height);
 
-  /* create the window and store the handle to it */
+  //  create the window and store the handle to it  
   wd = glutCreateWindow("Experiment with line drawing");
 
-  /* --- register callbacks with GLUT --- */
+  //  --- register callbacks with GLUT ---  
 
-  /* register function to handle window resizes */
-  glutReshapeFunc(reshape);
+  //  register function to handle window resizes  
+  glutReshapeFunc(reshapeCB);
 
-  /* register keyboard event processing function */
-  glutKeyboardFunc(kbd);
+  //  register keyboard event processing function  
+  glutKeyboardFunc(kbdCB);
 
-  /* register function that draws in the window */
-  glutDisplayFunc(display);
+  //  register function that draws in the window  
+  glutDisplayFunc(displayCB);
 
-  /* init GL */
+  //  init GL  
   glClearColor(1.0, 1.0, 1.0, 0.0);
   glColor3f(0.0, 0.0, 0.0);
   glLineWidth(3.0);
 
-  /* start the GLUT main loop */
+  // start the GLUT main loop  
   glutMainLoop();
 
   exit(0);
